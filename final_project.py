5a05b6d4757ca2257d0d9a31c5a6ace41abee2ca
tre-oconnell <156828922+tre-oconnell@users.noreply.github.com>
2024-04-05 00:09:53 -0400

    finished the create_background function

--- final_project.py
+++ final_project.py
@@ -7,32 +7,57 @@ CS1210-D
 import random
 
 def create_background(bg):
-    for i in range (10):
+    for _ in range (10):
         row = []
-        for i in range(20):
+        for _ in range(20):
             x = random.random()
             if x < 0.2:
                 row.append('B')
             elif x > 0.2:
                 row.append(0)
         bg.append(row)
-"""        
-#def complete_background(bg):
+
     #left and right check
-    for row in background:
-        for ind, each in enumerate(row):
-            if each != 'B':
+    for row in bg:
+        for row_ind, val in enumerate(row):
+            if val != 'B' and row_ind - 1 >= 0:
                 try:
-                    if row[ind - 1] == 'B':
-                        row[ind] += 1
-                    if row(ind + 1) == 'B':
-                        row[ind] += 1
+                    if row[row_ind - 1] == 'B':
+                        row[row_ind] += 1
+                    if row[row_ind + 1] == 'B':
+                        row[row_ind] += 1
                 except: 
                     IndexError
-    #up and down check
     
+    #up and down check 
+    for row_num, row in enumerate(bg):
+        for row_ind, row_val in enumerate(row):
+            if row_val != 'B' and row_num - 1 >= 0:
+                try:
+                    if bg[row_num - 1][row_ind] == 'B':
+                        bg[row_num][row_ind] += 1
+                    if bg[row_num + 1][row_ind] == 'B':
+                        bg[row_num][row_ind] += 1
+                except:
+                    IndexError
     #diagonal check
-"""
+    for row_num, row in enumerate(bg):
+        for row_ind, row_val in enumerate(row):
+            if row_val != 'B':
+                try:
+                    if row_num - 1 >= 0:
+                        if bg[row_num - 1][row_ind - 1] == 'B':
+                            bg[row_num][row_ind] += 1
+                        if bg[row_num - 1][row_ind + 1] == 'B':
+                            bg[row_num][row_ind] += 1
+                    if row_ind - 1 >= 0:
+                        if bg[row_num + 1][row_ind - 1] == 'B':
+                            bg[row_num][row_ind] += 1
+                        if bg[row_num + 1][row_ind + 1] == 'B':
+                            bg[row_num][row_ind] += 1
+                except:
+                    IndexError
+
 
 display = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -48,3 +73,14 @@ display = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 background = []
 
 create_background(background)
+"""
+for ind, row in enumerate(bg):
+    for row_ind, row_val in enumerate(row):
+        try:
+            if (ind - 1)[row_ind] == 'B':
+                ind[row_ind] += 1
+            if (ind + 1)[row_ind] == 'B':
+                ind[row_ind] += 1
+        except:
+            IndexError
+"""
