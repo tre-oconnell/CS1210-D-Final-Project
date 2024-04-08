37411646a66af554de1f1bbbb7413bbb6cd9e216
tre-oconnell <156828922+tre-oconnell@users.noreply.github.com>
2024-04-08 12:38:34 -0400

    Fixed bad logic of surrounding cell check

--- final_project.py
+++ final_project.py
@@ -14,7 +14,8 @@ root_tk.geometry("1600x880")
 root_tk.title("Minesweeper")
 
 restart_button = tkinter.Button(root_tk, text='Restart', height = 2, width = 10, command = lambda: restart())
-restart_button.grid(row=(11), column=5, columnspan=4)
+restart_button.grid(row=11, column=5, columnspan=4)
+
 
 
 
@@ -30,7 +31,7 @@ def create_background(bg):
         row = []
         for _ in range(20):
             x = random.random()
-            if x < 0.2:
+            if x <= 0.2:
                 row.append('B')
             elif x > 0.2:
                 row.append(0)
@@ -39,43 +40,35 @@ def create_background(bg):
     #left and right check
     for row in bg:
         for row_ind, val in enumerate(row):
-            if val != 'B' and row_ind - 1 >= 0:
-                try:
-                    if row[row_ind - 1] == 'B':
-                        row[row_ind] += 1
-                    if row[row_ind + 1] == 'B':
-                        row[row_ind] += 1
-                except: 
-                    IndexError
+            if val != 'B':
+                if row_ind - 1 >= 0 and row[row_ind - 1] == 'B':
+                    row[row_ind] += 1
+                if row_ind + 1 < len(row) and row[row_ind + 1] == 'B':
+                    row[row_ind] += 1
     
     #up and down check 
     for row_num, row in enumerate(bg):
         for row_ind, row_val in enumerate(row):
             if row_val != 'B':
-                try:
-                    if bg[row_num - 1][row_ind] == 'B' and row_num - 1 >= 0:
-                        bg[row_num][row_ind] += 1
-                    if bg[row_num + 1][row_ind] == 'B':
-                        bg[row_num][row_ind] += 1
-                except:
-                    IndexError
+                if row_num - 1 >= 0 and bg[row_num - 1][row_ind] == 'B':
+                    bg[row_num][row_ind] += 1
+                if row_num + 1 < len(bg) and bg[row_num + 1][row_ind] == 'B':
+                    bg[row_num][row_ind] += 1
+
     #diagonal check
     for row_num, row in enumerate(bg):
         for row_ind, row_val in enumerate(row):
             if row_val != 'B':
-                try:
-                    if row_num - 1 >= 0:
-                        if bg[row_num - 1][row_ind - 1] == 'B':
-                            bg[row_num][row_ind] += 1
-                        if bg[row_num - 1][row_ind + 1] == 'B':
-                            bg[row_num][row_ind] += 1
-                    if row_ind - 1 >= 0:
-                        if bg[row_num + 1][row_ind - 1] == 'B':
-                            bg[row_num][row_ind] += 1
-                        if bg[row_num + 1][row_ind + 1] == 'B':
-                            bg[row_num][row_ind] += 1
-                except:
-                    IndexError
+                if row_num - 1 >= 0:
+                    if row_ind - 1 >= 0 and bg[row_num - 1][row_ind - 1] == 'B':
+                        bg[row_num][row_ind] += 1
+                    if row_ind + 1 < len(row) and bg[row_num - 1][row_ind + 1] == 'B':
+                        bg[row_num][row_ind] += 1
+                if row_num + 1 < len(bg):
+                    if row_ind - 1 >= 0 and bg[row_num + 1][row_ind - 1] == 'B':
+                        bg[row_num][row_ind] += 1
+                    if row_ind + 1 < len(row) and bg[row_num + 1][row_ind + 1] == 'B':
+                        bg[row_num][row_ind] += 1
                     
 def create_display(disp):
     for _ in range(10):
@@ -83,13 +76,17 @@ def create_display(disp):
         for __ in range(20):
             row.append('')
         disp.append(row)
+        
 def create_buttons(bttns):
     for i in range(10):  # Rows
         row_buttons = []  # Create a list to store buttons in each row
         for j in range(20):  # Columns
             button = tkinter.Button(root_tk, text=display[i][j], height = 2, width = 1)
             button.grid(row=(i), column=j)
-            button.config(command=lambda i=i, j=j: reveal(i, j))
+            if display[i][j] == 'B':
+                button.config(command = lambda: disable(buttons))
+            else:
+                button.config(command=lambda i=i, j=j: reveal(i, j))
             row_buttons.append(button)  # Append button to row_buttons
         bttns.append(row_buttons)  # Append row_buttons to buttons
 
@@ -105,6 +102,11 @@ def restart():
     background = []
     buttons = []
     start()
+    
+def disable(buttons):
+    for row in buttons:
+        for button in row:
+            button.config(command = None)
 
 display = []
 background = []
@@ -112,5 +114,4 @@ buttons = []
 
 if __name__ == '__main__':
     start()
-
     root_tk.mainloop()
